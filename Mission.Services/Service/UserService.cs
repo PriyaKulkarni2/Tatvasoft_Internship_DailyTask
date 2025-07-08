f6dffbb5c888c6c2cea3a297ad01eb2a8eefad9e
PriyaKulkarni2 <avpril260604@gmail.com>
2025-07-08 14:20:37 +0530

    adding day 3

---
+++ Mission.Services/Service/UserService.cs
@@ -0,0 +1,45 @@
+﻿using Mission.Entities.ViewModels;
+using Mission.Entities.ViewModels.Login;
+using Mission.Repositories.IRepository;
+using Mission.Services.IService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission.Services.Service
+{
+    public class UserService(IUserRepository userRepository) : IUserService
+    {
+        private readonly IUserRepository _userRepository = userRepository;
+
+        //public Task<ResponseResult> LoginUser(UserLoginRequestModel model)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public async Task<ResponseResult> LoginUser(UserLoginRequestModel model)
+        {
+            var (response, message) = await _userRepository.LoginUser(model);
+
+            var result = new ResponseResult()
+            {
+                Data = response,
+                Message = message
+            };
+
+            if (response == null)
+            {
+                result.Result = ResponseStatus.Error;
+            }
+            else
+            {
+
+                result.Result = ResponseStatus.Success;
+            }
+
+            return result;
+        }
+    }
+}
